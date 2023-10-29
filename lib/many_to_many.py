19ab9dbb356352e29da823865265f3ac7b46b926
Brian Richie Sr <brianrichiesr@gmail.com>
2023-10-29 11:08:45 -0700

    complete lab

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,80 @@
-class Author:
-    pass
+from datetime import datetime
 
+class Author:
+
+    def __init__(self, name):
+        self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+    
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in Contract.all if contract.author == self]
+    
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum([contract.royalties for contract in Contract.all if contract.author == self])
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        self._title = title
+    
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in Contract.all if contract.book == self]
 
 
 class Contract:
-    pass
+
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise TypeError("Author needs to be type Author")
+        else:
+            self.author = author
+
+        if not isinstance(book, Book):
+            raise TypeError("Book needs to be type Book")
+        else:
+            self.book = book
+        
+        if not isinstance(date, str):
+            raise TypeError("Date needs to be type str")
+        else:
+            self.date = date
+
+        if not isinstance(royalties, int):
+            raise TypeError("Royalties needs to be type int")
+        else:
+            self.royalties = royalties
+
+        type(self).all.append(self)
+    
+    @classmethod
+    def contracts_by_date(cls, date):
+        # The test for this is badly written. The following passes
+        #   the test even though it does not sort
+        # return [contract for contract in cls.all if contract.date == date]
+        return sorted([contract for contract in cls.all if contract.date == date], key=lambda contract: datetime.strptime(contract.date, "%m/%d/%Y"))
+            
